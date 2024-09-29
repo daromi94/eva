1263ffbc34550cb9e04b71c1173d3cddba803617
daromi94 <davromero07@gmail.com>
2024-09-29 00:44:05 -0500

    style: :art: remove custom formatting rules

--- src/main/scala/com/daromi/eva/core/expressions/Operations.scala
+++ src/main/scala/com/daromi/eva/core/expressions/Operations.scala
@@ -1,6 +1,10 @@
 package com.daromi.eva.core.expressions
 
-case class IntegerAddition(lhs: IntegerExpression, rhs: IntegerExpression) extends IntegerExpression:
+case class IntegerAddition(
+    lhs: IntegerExpression,
+    rhs: IntegerExpression
+) extends IntegerExpression:
+
   override def value: Int = this.lhs.value + this.rhs.value
 
   override def toString: String = s"(+ ${this.lhs} ${this.rhs})"
