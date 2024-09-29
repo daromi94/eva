9f30b58ffc6507ee15eaaf90430af2e5d7a19c36
daromi94 <davromero07@gmail.com>
2024-09-29 09:42:25 -0500

    feat: :sparkles: add integer multiplication and subtraction expressions

--- src/main/scala/com/daromi/eva/core/expressions/Operations.scala
+++ src/main/scala/com/daromi/eva/core/expressions/Operations.scala
@@ -8,3 +8,21 @@ final case class IntegerAddition(
   override def value: Int = this.lhs.value + this.rhs.value
 
   override def toString: String = s"(+ ${this.lhs} ${this.rhs})"
+
+final case class IntegerSubtraction(
+    lhs: IntegerExpression,
+    rhs: IntegerExpression
+) extends IntegerExpression:
+
+  override def value: Int = this.lhs.value - this.rhs.value
+
+  override def toString: String = s"(- ${this.lhs} ${this.rhs})"
+
+final case class IntegerMultiplication(
+    lhs: IntegerExpression,
+    rhs: IntegerExpression
+) extends IntegerExpression:
+
+  override def value: Int = this.lhs.value * this.rhs.value
+
+  override def toString: String = s"(* ${this.lhs} ${this.rhs})"
