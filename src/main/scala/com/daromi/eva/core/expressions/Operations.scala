3dd89d6e20fcd2c8f69e27104a9b48e261e6d625
daromi94 <davromero07@gmail.com>
2024-10-06 18:40:01 -0500

    refactor: :recycle: use descriptive names for integer operations

--- src/main/scala/com/daromi/eva/core/expressions/Operations.scala
+++ src/main/scala/com/daromi/eva/core/expressions/Operations.scala
@@ -2,58 +2,50 @@ package com.daromi.eva.core.expressions
 
 import com.daromi.eva.core.Environment
 
-final case class IntegerAdd(
+final case class IntegerAddition(
     lhs: Expression[Int],
     rhs: Expression[Int]
 ) extends Expression[Int]:
 
   override def evaluate(environment: Environment): Int =
     val left = this.lhs.evaluate(environment)
-
     val right = this.rhs.evaluate(environment)
-
     left + right
 
   override def toString: String = s"(+ ${this.lhs} ${this.rhs})"
 
-final case class IntegerSubtract(
+final case class IntegerSubtraction(
     lhs: Expression[Int],
     rhs: Expression[Int]
 ) extends Expression[Int]:
 
   override def evaluate(environment: Environment): Int =
     val left = this.lhs.evaluate(environment)
-
     val right = this.rhs.evaluate(environment)
-
     left - right
 
   override def toString: String = s"(- ${this.lhs} ${this.rhs})"
 
-final case class IntegerMultiply(
+final case class IntegerMultiplication(
     lhs: Expression[Int],
     rhs: Expression[Int]
 ) extends Expression[Int]:
 
   override def evaluate(environment: Environment): Int =
     val left = this.lhs.evaluate(environment)
-
     val right = this.rhs.evaluate(environment)
-
     left * right
 
   override def toString: String = s"(* ${this.lhs} ${this.rhs})"
 
-final case class IntegerDivide(
+final case class IntegerDivision(
     lhs: Expression[Int],
     rhs: Expression[Int]
 ) extends Expression[Int]:
 
   override def evaluate(environment: Environment): Int =
     val left = this.lhs.evaluate(environment)
-
     val right = this.rhs.evaluate(environment)
-
     left / right
 
   override def toString: String = s"(/ ${this.lhs} ${this.rhs})"
