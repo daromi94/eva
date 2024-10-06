eb9921833e13ffaec774b0bade2942f1a3b0f2b3
daromi94 <davromero07@gmail.com>
2024-10-06 18:38:18 -0500

    refactor: :recycle: remove superfluous string interpolation

--- src/main/scala/com/daromi/eva/core/expressions/Literals.scala
+++ src/main/scala/com/daromi/eva/core/expressions/Literals.scala
@@ -5,12 +5,12 @@ import com.daromi.eva.core.Environment
 final case class BooleanLiteral(b: Boolean) extends Expression[Boolean]:
   override def evaluate(environment: Environment): Boolean = this.b
 
-  override def toString: String = s"${this.b}"
+  override def toString: String = this.b.toString
 
 final case class IntegerLiteral(n: Int) extends Expression[Int]:
   override def evaluate(environment: Environment): Int = this.n
 
-  override def toString: String = s"${this.n}"
+  override def toString: String = this.n.toString
 
 final case class StringLiteral(str: String) extends Expression[String]:
   override def evaluate(environment: Environment): String = this.str
