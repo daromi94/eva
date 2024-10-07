6996f60fa94d50e01e5da2fae4b1ec5a70ba21d3
daromi94 <davromero07@gmail.com>
2024-10-06 20:10:37 -0500

    fix: :bug: enable variable assignment within child scopes

--- src/main/scala/com/daromi/eva/interpreter/Environment.scala
+++ src/main/scala/com/daromi/eva/interpreter/Environment.scala
@@ -8,14 +8,18 @@ final class Environment private (
     private val record: mutable.Map[Identifier, Any] = mutable.Map.empty,
     private val parent: Option[Environment] = None
 ):
+  def exists(identifier: Identifier): Boolean =
+    this.record.contains(identifier)
+
+  def resolve(identifier: Identifier): Option[Environment] =
+    if exists(identifier) then Some(this)
+    else this.parent.flatMap(_.resolve(identifier))
+
   def set(identifier: Identifier, value: Any): Unit =
     this.record.update(identifier, value)
 
   def get(identifier: Identifier): Option[Any] =
-    this.record.get(identifier) orElse this.parent.flatMap(_.get(identifier))
-
-  def exists(identifier: Identifier): Boolean =
-    this.record.contains(identifier)
+    resolve(identifier).flatMap(_.record.get(identifier))
 
 object Environment:
   def empty: Environment = Environment()
--- src/main/scala/com/daromi/eva/interpreter/expressions/Variables.scala
+++ src/main/scala/com/daromi/eva/interpreter/expressions/Variables.scala
@@ -6,7 +6,8 @@ type Identifier = String
 
 final case class VariableDeclaration[T](identifier: Identifier, expression: Expression[T]) extends Expression[T]:
   override def evaluate(environment: Environment): T =
-    if environment.exists(identifier) then throw new RuntimeException(s"'${this.identifier}' already defined in scope")
+    if environment.exists(this.identifier) then
+      throw new RuntimeException(s"'${this.identifier}' already defined in scope")
     else
       val value = this.expression.evaluate(environment)
       environment.set(this.identifier, value)
@@ -16,9 +17,13 @@ final case class VariableDeclaration[T](identifier: Identifier, expression: Expr
 
 final case class VariableAssignment[T](identifier: Identifier, expression: Expression[T]) extends Expression[T]:
   override def evaluate(environment: Environment): T =
-    val value = this.expression.evaluate(environment)
-    environment.set(this.identifier, value)
-    value
+    environment.resolve(this.identifier) match
+      case Some(scope) =>
+        val value = this.expression.evaluate(environment)
+        scope.set(this.identifier, value)
+        value
+
+      case None => throw new RuntimeException(s"'${this.identifier}' not defined in scope chain")
 
   override def toString: String = s"(set ${this.identifier} ${this.expression})"
 
@@ -26,6 +31,6 @@ final case class VariableLookup[T](identifier: Identifier) extends Expression[T]
   override def evaluate(environment: Environment): T =
     environment.get(this.identifier) match
       case Some(value) => value.asInstanceOf[T]
-      case None        => throw new RuntimeException(s"'${this.identifier}' not defined in scope")
+      case None        => throw new RuntimeException(s"'${this.identifier}' not defined in scope chain")
 
   override def toString: String = this.identifier
