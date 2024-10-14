bd64d4ba5cd370f533008b364855df18fbe699d2
daromi94 <davromero07@gmail.com>
2024-10-14 14:39:24 -0500

    refactor: :recycle: use descriptive names for variable operands

--- src/main/scala/com/daromi/eva/interpreter/expressions/Variables.scala
+++ src/main/scala/com/daromi/eva/interpreter/expressions/Variables.scala
@@ -6,34 +6,34 @@ type Identifier = String
 
 final case class VariableDeclaration[T](
     identifier: Identifier,
-    expression: Expression[T]
+    rhs: Expression[T]
 ) extends Expression[T]:
 
   override def evaluate(environment: Environment): T =
     if environment.exists(this.identifier) then
       throw new RuntimeException(s"'${this.identifier}' already defined in scope")
     else
-      val value = this.expression.evaluate(environment)
+      val value = this.rhs.evaluate(environment)
       environment.set(this.identifier, value)
       value
 
-  override def toString: String = s"(var ${this.identifier} ${this.expression})"
+  override def toString: String = s"(var ${this.identifier} ${this.rhs})"
 
 final case class VariableAssignment[T](
     identifier: Identifier,
-    expression: Expression[T]
+    rhs: Expression[T]
 ) extends Expression[T]:
 
   override def evaluate(environment: Environment): T =
     environment.resolve(this.identifier) match
       case Some(scope) =>
-        val value = this.expression.evaluate(environment)
+        val value = this.rhs.evaluate(environment)
         scope.set(this.identifier, value)
         value
 
       case None => throw new RuntimeException(s"'${this.identifier}' not defined in scope chain")
 
-  override def toString: String = s"(set ${this.identifier} ${this.expression})"
+  override def toString: String = s"(set ${this.identifier} ${this.rhs})"
 
 final case class VariableLookup[T](
     identifier: Identifier
