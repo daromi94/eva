87d66766caaba878a8d3a37d732e82bc695a9481
daromi94 <davromero07@gmail.com>
2024-10-14 14:40:45 -0500

    refactor: :recycle: add type annotation to block

--- src/main/scala/com/daromi/eva/interpreter/expressions/Block.scala
+++ src/main/scala/com/daromi/eva/interpreter/expressions/Block.scala
@@ -2,14 +2,15 @@ package com.daromi.eva.interpreter.expressions
 
 import com.daromi.eva.interpreter.Environment
 
-final case class Block(
+final case class Block[T](
     expressions: Seq[Expression[Any]]
-) extends Expression[Any]:
+) extends Expression[T]:
 
-  override def evaluate(environment: Environment): Any =
+  override def evaluate(environment: Environment): T =
     if this.expressions.isEmpty then throw new RuntimeException("empty block not allowed")
     else
-      val scope = Environment.childOf(environment)
-      this.expressions.map(_.evaluate(scope)).last
+      val scope  = Environment.childOf(environment)
+      val values = this.expressions.map(_.evaluate(scope))
+      values.last.asInstanceOf[T]
 
   override def toString: String = s"(begin ${this.expressions.mkString(" ")})"
