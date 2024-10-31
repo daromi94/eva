72ac562bc4769107bfb76b3901831d1c7682bf73
daromi94 <davromero07@gmail.com>
2024-10-30 20:09:21 -0500

    refactor: :recycle: group raw symbols

---
+++ src/main/scala/com/daromi/eva/core/lexer/RawSymbols.scala
@@ -0,0 +1,9 @@
+package com.daromi.eva.core.lexer
+
+private object RawSymbols:
+  val OpenParenthesis  = '('
+  val CloseParenthesis = ')'
+  val DecimalPoint     = '.'
+  val Underscore       = '_'
+  val DoubleQuote      = '"'
+  val Escape           = '\\'
