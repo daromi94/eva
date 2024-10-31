af4d3515bbec11fb06d1728a33101d9fcb762b33
daromi94 <davromero07@gmail.com>
2024-10-30 20:31:47 -0500

    refactor: :recycle: use raw symbols in scanner

--- src/main/scala/com/daromi/eva/core/lexer/Scanner.scala
+++ src/main/scala/com/daromi/eva/core/lexer/Scanner.scala
@@ -2,17 +2,10 @@ package com.daromi.eva.core.lexer
 
 private type Lexeme = String
 
-object Scanner:
+private object Scanner:
   private type Buffer = Array[Char]
   private type Cursor = Int
   private type Index  = Int
-
-  private val OpenParenthesis  = '('
-  private val CloseParenthesis = ')'
-  private val Quote            = '"'
-  private val Escape           = '\\'
-  private val DecimalPoint     = '.'
-  private val Underscore       = '_'
 
   def scan(source: Source): Seq[Lexeme] =
     if source.isBlank then
@@ -35,11 +28,11 @@ object Scanner:
     val first = buffer(cursor)
 
     first match
-      case OpenParenthesis | CloseParenthesis => munch(delimiterEndsAt)
-      case Quote                              => munch(stringEndsAt)
-      case c if c.isDigit                     => munch(numberEndsAt)
-      case c if c.isUnicodeIdentifierStart    => munch(symbolEndsAt)
-      case c if c.isWhitespace                => ignore(cursor)
+      case RawSymbols.OpenParenthesis | RawSymbols.CloseParenthesis => munch(delimiterEndsAt)
+      case RawSymbols.DoubleQuote                                   => munch(stringEndsAt)
+      case c if c.isDigit                                           => munch(numberEndsAt)
+      case c if c.isUnicodeIdentifierStart                          => munch(symbolEndsAt)
+      case c if c.isWhitespace                                      => ignore(cursor)
 
   private def muncher(buffer: Buffer, cursor: Cursor)(endsAt: (Buffer, Cursor) => Index): LexemeResult =
     val end = endsAt(buffer, cursor)
@@ -55,27 +48,27 @@ object Scanner:
   private def delimiterEndsAt(buffer: Buffer, cursor: Cursor): Index = cursor
 
   private def stringEndsAt(buffer: Buffer, cursor: Cursor): Index =
-    if cursor > 0 && buffer(cursor - 1) == Escape then
+    if cursor > 0 && buffer(cursor - 1) == RawSymbols.Escape then
       -1
     else
       var i = cursor + 1
 
       while i < buffer.length do
-        if buffer(i - 1) != Escape && buffer(i) == Quote then
+        if buffer(i - 1) != RawSymbols.Escape && buffer(i) == RawSymbols.DoubleQuote then
           return i
         i += 1
 
       -1
 
   private def numberEndsAt(buffer: Buffer, cursor: Cursor): Index =
-    val next = buffer.indexWhere(c => !c.isDigit && c != DecimalPoint && c != Underscore, cursor)
+    val next = buffer.indexWhere(c => !c.isDigit && c != RawSymbols.DecimalPoint && c != RawSymbols.Underscore, cursor)
     if next == -1 then
       -1
     else
       next - 1
 
   private def symbolEndsAt(buffer: Buffer, cursor: Cursor): Index =
-    val next = buffer.indexWhere(c => c.isWhitespace || c == CloseParenthesis, cursor)
+    val next = buffer.indexWhere(c => c.isWhitespace || c == RawSymbols.CloseParenthesis, cursor)
     if next == -1 then
       -1
     else
