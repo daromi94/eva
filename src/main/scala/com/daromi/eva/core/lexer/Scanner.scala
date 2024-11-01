049d62a26c4e5e6bb9ab8653dff7aaeecaf34873
daromi94 <davromero07@gmail.com>
2024-10-31 22:41:36 -0500

    feat: :sparkles: munch numbers

--- src/main/scala/com/daromi/eva/core/lexer/Scanner.scala
+++ src/main/scala/com/daromi/eva/core/lexer/Scanner.scala
@@ -16,32 +16,41 @@ final private class Scanner private (
     val lexemes = ArrayBuffer.empty[Lexeme]
 
     while cursor < buffer.length do
-      advance().foreach { lexemes += _ }
+      if current.isWhitespace then
+        skipOne()
+      else
+        val lexeme = advance()
+        lexemes += lexeme
 
     lexemes.toSeq
 
   private def current: Char = buffer(cursor)
 
-  private def advance(): Option[Lexeme] =
-    assume(0 <= cursor && cursor < buffer.length - 1)
+  private def skipOne(): Unit =
+    assume(0 <= cursor && cursor < buffer.length, s"cursor $cursor out of bounds")
+
+    cursor += 1
+
+  private def advance(): Lexeme =
+    assume(0 <= cursor && cursor < buffer.length, s"cursor $cursor out of bounds")
 
     current match
-      case OpenParenthesis | CloseParenthesis => munchOne()
-      case Plus | Minus | Multiply | Divide   => munchOne()
+      case OpenParenthesis | CloseParenthesis => takeOne()
+      case Plus | Minus | Multiply | Divide   => takeOne()
       case DoubleQuote                        => munchString()
-      case c if c.isDigit                     => None
-      case c if c.isUnicodeIdentifierStart    => None
-      case c if c.isWhitespace                => ignore()
+      case c if c.isDigit                     => munchNumber()
+      case c                                  => throw RuntimeException(s"invalid character '$c' at cursor $cursor")
 
-  private def munchOne(): Option[Lexeme] =
-    assume(0 <= cursor && cursor < buffer.length - 1)
+  private def takeOne(): Lexeme =
+    assume(0 <= cursor && cursor < buffer.length, s"cursor $cursor out of bounds")
 
     val lexeme = current.toString
     cursor += 1
-    Some(lexeme)
+    lexeme
 
-  private def munchString(): Option[Lexeme] =
-    assume(0 <= cursor && cursor < buffer.length - 1)
+  private def munchString(): Lexeme =
+    assume(0 <= cursor && cursor < buffer.length, s"cursor $cursor out of bounds")
+    assume(current == DoubleQuote, s"double quote expected at start of string, at cursor $cursor")
 
     val start = cursor
     var i     = start + 1
@@ -55,15 +64,42 @@ final private class Scanner private (
     if found then
       val lexeme = buffer.slice(start, i).mkString
       cursor = i
-      Some(lexeme)
+      lexeme
     else
-      None
+      throw RuntimeException(s"unterminated string at cursor $cursor")
 
-  private def ignore(): Option[Lexeme] =
-    assume(0 <= cursor && cursor < buffer.length - 1)
+  private def munchNumber(): Lexeme =
+    assume(0 <= cursor && cursor < buffer.length, s"cursor $cursor out of bounds")
+    assume(current.isDigit, s"digit expected at start of number, at cursor $cursor")
 
-    cursor += 1
-    None
+    val partial = ArrayBuffer(current)
+    var decimal = false
+
+    val start = cursor
+    var i     = start + 1
+    var found = false
+
+    while !found && i < buffer.length do
+      buffer(i) match
+        case c if c.isDigit || c == Underscore => partial += c
+
+        case c if c == DecimalPoint =>
+          if !decimal then
+            partial += c
+            decimal = true
+          else
+            throw RuntimeException(s"multiple decimal points in number at cursor $cursor")
+
+        case _ => found = true
+
+      i += 1
+
+    if partial.last == Underscore then
+      throw RuntimeException(s"trailing underscore in number at cursor $cursor")
+
+    val lexeme = partial.mkString
+    cursor = if found then i - 1 else i
+    lexeme
 
 private object Scanner:
 
